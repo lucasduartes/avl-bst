c30be20e0dabb9d76829d6a86034b3b8aa2d9601
Marcelo <mvrozanti@hotmail.com>
2023-04-23 13:32:46 -0300

    Adicionar gráfico da progressão da altura de cada árvore e adicionar legendas

--- plot.py
+++ plot.py
@@ -12,28 +12,50 @@ bst = BinaryTree()
  average_read_time_avl, 
  total_time_read_avl, 
  time_to_insert_seconds_avl, 
- height_avl) = time_tree(df, avl)
+ heights_avl) = time_tree(df, avl)
 
 (average_insertion_time_bst, 
  total_time_write_bst, 
  average_read_time_bst, 
  total_time_read_bst, 
  time_to_insert_seconds_bst, 
- height_bst) = time_tree(df, bst)
+ heights_bst) = time_tree(df, bst)
 
-fig, axs = plt.subplots(2, 3, figsize=(12,10))
+fig, axs = plt.subplots(2, 4, figsize=(16,11))
 axs[0, 0].bar(['AVL', 'BST'], [total_time_write_avl, total_time_write_bst])
 axs[0, 0].set_title('Total insertion time')
+axs[0, 0].set_ylabel('(s)', rotation=0, labelpad=10)
+
 axs[0, 1].bar(['AVL', 'BST'], [average_insertion_time_avl, average_insertion_time_bst])
 axs[0, 1].set_title('Average insertion time')
+axs[0, 1].set_ylabel('(s)', rotation=0, labelpad=10)
+
 axs[1, 0].bar(['AVL', 'BST'], [total_time_read_avl, total_time_read_bst])
 axs[1, 0].set_title('Total read time')
+axs[1, 0].set_ylabel('(s)', rotation=0, labelpad=10)
+
 axs[1, 1].bar(['AVL', 'BST'], [average_read_time_avl, average_read_time_bst])
 axs[1, 1].set_title('Average read time')
+axs[1, 1].set_ylabel('(s)', rotation=0, labelpad=10)
+
 axs[0, 2].plot(time_to_insert_seconds_avl)
 axs[0, 2].set_title('Time to insert (AVL)')
+axs[0, 2].set_ylabel('(s)', rotation=0, labelpad=10)
+axs[0, 2].set_xlabel('Insertion Number')
+
 axs[1, 2].plot(time_to_insert_seconds_bst)
 axs[1, 2].set_title('Time to insert (BST)')
-plt.suptitle('Time units are in seconds', fontsize=10, x=0.95, y=0.013, ha='right')
+axs[1, 2].set_ylabel('(s)', rotation=0, labelpad=10)
+axs[1, 2].set_xlabel('Insertion Number')
+
+axs[0, 3].plot(heights_avl)
+axs[0, 3].set_title('AVL Tree Heights')
+axs[0, 3].set_xlabel('Insertion Number')
+
+axs[1, 3].plot(heights_bst)
+axs[1, 3].set_title('BST Heights')
+axs[1, 3].set_xlabel('Insertion Number')
+
 plt.tight_layout()
 plt.savefig('comparação.png')
+
--- time_tree.py
+++ time_tree.py
@@ -6,15 +6,16 @@ from tree_height import get_tree_height
 def time_tree(df, tree):
     t0 = time()
     time_to_insert_seconds = []
+    tree_heights = []
     for _, instant, usd in df.itertuples():
         tn = time()
         tree.insert(instant, usd)
         t = time()
+        tree_heights += [get_tree_height(tree)]
         time_to_insert_seconds += [t - tn]
     tf = time()
     total_time_write = tf - t0
     average_insertion_time = sum(time_to_insert_seconds)/len(time_to_insert_seconds)
-    tree_height = get_tree_height(tree)
     time_to_read_seconds = []
     sample_count = len(df)//2
     sample_instants = list(df.sample(sample_count)['Open Time'])
@@ -27,4 +28,4 @@ def time_tree(df, tree):
     tf = time()
     total_time_read = tf - t0
     average_read_time = sum(time_to_read_seconds)/len(time_to_read_seconds)
-    return average_insertion_time, total_time_write, average_read_time, total_time_read, time_to_insert_seconds, tree_height
+    return average_insertion_time, total_time_write, average_read_time, total_time_read, time_to_insert_seconds, tree_heights
